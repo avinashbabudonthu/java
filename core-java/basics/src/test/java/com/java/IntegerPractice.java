75146251a6bd7a56326954e7563bb39d65de8271
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2022-06-02 20:31:43 +0530

    integer practice

--- core-java/basics/src/test/java/com/java/IntegerPractice.java
+++ core-java/basics/src/test/java/com/java/IntegerPractice.java
@@ -1,37 +1,39 @@
 package com.java;
 
+import org.junit.Test;
+
 public class IntegerPractice {
-  
-  /**
-     * int -> Integer
-     * auto boxing can do this but valueOf will cache the value for better space & time performance
-      */
-    @Test
-    public void valueOf(){
-        int i = 10;
-        Integer j = Integer.valueOf(i);
-        System.out.println(j);
-    }
 
-    /**
-     * String to signed int
-     */
-    @Test
-    public void parseInt(){
-        int i1 = Integer.parseInt("100");
-        System.out.println(i1);
-        int i2 = Integer.parseInt("-101");
-        System.out.println(i2);
-    }
+	/**
+	 * int -> Integer
+	 * auto boxing can do this but valueOf will cache the value for better space & time performance
+	  */
+	@Test
+	public void valueOf() {
+		int i = 10;
+		Integer j = Integer.valueOf(i);
+		System.out.println(j);
+	}
 
-    /**
-     * String to unsigned int
-     */
-    @Test
-    public void parseUnsignedInt(){
-        int i1 = Integer.parseUnsignedInt("100");
-        System.out.println(i1); // 100
-        int i2 = Integer.parseUnsignedInt("-101"); // java.lang.NumberFormatException: Illegal leading minus sign on unsigned string -101.
-    }
-  
+	/**
+	 * String to signed int
+	 */
+	@Test
+	public void parseInt() {
+		int i1 = Integer.parseInt("100");
+		System.out.println(i1);
+		int i2 = Integer.parseInt("-101");
+		System.out.println(i2);
+	}
+
+	/**
+	 * String to unsigned int
+	 */
+	@Test
+	public void parseUnsignedInt() {
+		int i1 = Integer.parseUnsignedInt("100");
+		System.out.println(i1); // 100
+		int i2 = Integer.parseUnsignedInt("-101"); // java.lang.NumberFormatException: Illegal leading minus sign on unsigned string -101.
+	}
+
 }
