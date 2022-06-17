34a5c3d895a56fb8f8f4bbb50f38f629755e354b
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2022-06-17 12:44:59 +0530

    comments added

--- core-java/collections/src/test/java/com/util/ArraysPractice.java
+++ core-java/collections/src/test/java/com/util/ArraysPractice.java
@@ -256,12 +256,22 @@ public class ArraysPractice {
 		System.out.println("Arrays.deepEquals(array1, array3) : " + Arrays.deepEquals(array1, array3));
 	}
 
+	/**
+     * Returns a hash code based on the contents of the specified array. If the array contains other arrays as elements, the hash code is based on their identities rather than their contents. It is therefore acceptable to invoke this method on an array that contains itself as an element, either directly or indirectly through one or more levels of arrays.
+     * For any two arrays a and b such that Arrays.equals(a, b), it is also the case that Arrays.hashCode(a) == Arrays.hashCode(b).
+     * The value returned by this method is equal to the value that would be returned by Arrays.asList(a).hashCode(), unless a is null, in which case 0 is returned.
+     */
 	@Test
 	public void hashCodePractice() {
 		Integer[] array1 = new Integer[] { 1, 2, 3, 4, 5 };
 		System.out.println("Arrays.hashCode(array1) : " + Arrays.hashCode(array1));
 	}
 
+	    /**
+     * Returns a hash code based on the "deep contents" of the specified array. If the array contains other arrays as elements, the hash code is based on their contents and so on, ad infinitum. It is therefore unacceptable to invoke this method on an array that contains itself as an element, either directly or indirectly through one or more levels of arrays. The behavior of such an invocation is undefined.
+     * For any two arrays a and b such that Arrays.deepEquals(a, b), it is also the case that Arrays.deepHashCode(a) == Arrays.deepHashCode(b).
+     * The computation of the value returned by this method is similar to that of the value returned by List.hashCode() on a list containing the same elements as a in the same order, with one difference: If an element e of a is itself an array, its hash code is computed not by calling e.hashCode(), but as by calling the appropriate overloading of Arrays.hashCode(e) if e is an array of a primitive type, or as by calling Arrays.deepHashCode(e) recursively if e is an array of a reference type. If a is null, this method returns 0
+     */
 	@Test
 	public void deepHashCodePractice() {
 		Integer[] array1 = new Integer[] { 1, 2, 3, 4, 5 };
