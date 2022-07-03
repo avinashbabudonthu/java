9742a75a6a191457715c3ad3084327c07c9811a1
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2022-07-03 20:33:33 +0530

    shutdown hook

---
+++ core-java/basics/src/main/java/com/java/lang/ShutdownHook1.java
@@ -0,0 +1,26 @@
+package com.java.lang;
+
+public class ShutdownHook1 {
+
+    /**
+     * program started
+     * waiting 3 seconds
+     * program completed
+     * shutdown hook 1
+     * shutdown hook 2
+     */
+    public static void main(String[] args) throws Exception {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("shutdown hook 1");
+        }));
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("shutdown hook 2");
+        }));
+
+        System.out.println("program started");
+        System.out.println("waiting 3 seconds");
+        Thread.sleep(1000 * 3);
+        System.out.println("program completed");
+    }
+}
