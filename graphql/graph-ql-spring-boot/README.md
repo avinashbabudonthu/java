f6c1948aa28d6839579f6de6e3968b7f6a3983cc
Donthu Avinash Babu <Avinash.Donthu@adp.com>
2023-01-19 11:45:39 +0530

    graphql practice

--- graphql/graph-ql-spring-boot/README.md
+++ graphql/graph-ql-spring-boot/README.md
@@ -2,7 +2,9 @@
 * Dependencies - [pom.xml](pom.xml)
 * Application properties - [application.yml](src/main/resources/application.yml)
 * Graphql schema - [query.graphqls](src/main/resources/schema/schema.graphqls)
-    * firstQuery - [firstQuery method](src/main/java/com/java/query/Query.java)
+  * firstQuery - [firstQuery method](src/main/java/com/java/query/Query.java)
+  * fullName - [fullName method](src/main/java/com/java/query/Query.java)
+  * studentRequest - [studentRequest method](src/main/java/com/java/query/Query.java)
 
 # Run App
 * Run main class - [App](src/main/java/com/java/App.java)
--- graphql/graph-ql-spring-boot/src/main/java/com/java/query/Query.java
+++ graphql/graph-ql-spring-boot/src/main/java/com/java/query/Query.java
@@ -1,5 +1,6 @@
 package com.java.query;
 
+import com.java.model.Student;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,20 @@ public class Query implements GraphQLQueryResolver {
 
     public String firstQuery(){
         return "Hello world";
+    }
+
+    public String fullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
+
+    /**
+     * Query with student object as request body
+     *
+     * @param student
+     * @return
+     */
+    public String studentRequest(Student student){
+        return student.getId() + " " + student.getFirstName() + " " + student.getLastName();
     }
 
 }
