953a516dbdd8d96ee0231cbcbe9c3205b3741757
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2023-12-09 00:21:51 +0530

    terraform

--- java/terraform/azure/create-resource-group.tf
+++ java/terraform/azure/create-resource-group.tf
@@ -9,10 +9,10 @@ terraform {
 
 # azure configurations
 provider "azurerm" {
-  subscription_id = "3deb56dc-759b-48fc-99e4-236f7b158829"
-  tenant_id = "c9711627-cc03-474b-849c-c90908abcd26"
-  client_id = "e61e576d-0c96-4204-8090-856eaf6c65c2"
-  client_secret = "y218Q~KetdbAPAfwv_Ok_MT5h7FRTEvbPnROka7p"
+  subscription_id = "xxxxxxxxxxx"
+  tenant_id = "xxxxxxxxxxx"
+  client_id = "xxxxxxxxxxx"
+  client_secret = "xxxxxxxxxxx"
   features {}
 }
 
