4c8efebef6cab92ebf232115862b58aa731832e4
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2024-07-15 23:32:06 +0530

    spring security

--- java-v2/spring-security/scrible/spring-security-app-01/src/main/java/com/spring/security/config/AppSecurityConfig.java
+++ java-v2/spring-security/scrible/spring-security-app-01/src/main/java/com/spring/security/config/AppSecurityConfig.java
@@ -2,14 +2,28 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class AppSecurityConfig {
 
+    /**
+     * Refer {@link org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration} class defaultSecurityFilterChain method for default implementation
+     * @param http {@link HttpSecurity}
+     * @return {@link SecurityFilterChain}
+     * @throws {@link Exception} propagates exceptions from authorizeHttpRequests, formLogin, httpBasic methods
+     */
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
@@ -20,6 +34,29 @@ public class AppSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    /**
+     * {@link User} extends {@link UserDetails}
+     * {@link InMemoryUserDetailsManager} extends {@link UserDetailsService}
+     * @return In memory user details object with our users credentials
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // we need to prefix password with {noop} if we do not provide PasswordEncoder bean
+        UserDetails user1 = User.withUsername("user1").password("{noop}12345").roles("read").build();
+        UserDetails user2 = User.withUsername("user2").password("{bcrypt}$2a$12$Oxfjl53Ch6eZrQRK5ZCTTe.uTrbsXOoGPumEYlQWzi2wTADpzlkTG").roles("admin").build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 
 }
