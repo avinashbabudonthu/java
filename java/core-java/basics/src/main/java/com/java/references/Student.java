41c6a5a6c0a967d7459c1829f28b6a2f30de3969
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2025-02-01 22:49:29 +0530

    java

--- java/core-java/basics/src/main/java/com/java/references/Student.java
+++
@@ -1,4 +0,0 @@
-package com.java.references;
-
-public class Student {
-}
--- java/core-java/basics/src/main/java/com/java/util/stream/IntStreamPractice.java
+++
@@ -1,9 +0,0 @@
-package com.java.util.stream;
-
-/**
- * References:
- *
- * https://www.tutorialspoint.com/intstream-range-method-in-java
- */
-public class IntStreamPractice {
-}
