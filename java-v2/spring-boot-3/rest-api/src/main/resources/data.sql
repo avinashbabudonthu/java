3e7dd065270214446b99e0bf39ba03cf051e1a4e
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2025-03-08 16:46:53 +0530

    java

--- java-v2/spring-boot-3/rest-api/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into employee (id, name, joining_date) values (random_uuid(), 'a', current_date());
-insert into employee (id, name, joining_date) values (random_uuid(), 'b', current_date());
-insert into employee (id, name, joining_date) values (random_uuid(), 'c', current_date());
